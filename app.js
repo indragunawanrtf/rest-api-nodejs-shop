976deb66319184753f068003d473db1ca59c6734
Indra Gunawan <indragunawanrtf@gmail.com>
2018-04-15 11:15:15 +0700

    Handling Error

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-const productRoutes = require('./api/routes/product');
-const orderRoutes = require('./api/routes/order');
+const productRoutes = require("./api/routes/product");
+const orderRoutes = require("./api/routes/order");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+
+// Handling Error
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
