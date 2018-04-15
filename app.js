733d98214020d5ab2cac59c629562192fc016c13
Indra Gunawan <indragunawanrtf@gmail.com>
2018-04-15 11:03:40 +0700

    Add packages morgan

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const productRoutes = require('./api/routes/product');
 const orderRoutes = require('./api/routes/order');
+
+app.use(morgan('dev'));
 
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
